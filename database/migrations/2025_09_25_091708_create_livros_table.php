7643cc2af8d7f0d97bc013541dc6b1e4617b88f8
Dmpl1909 <dmpl.gpsi.2019@gmail.com>
2025-09-25 11:17:11 +0100

    base

---
+++ database/migrations/2025_09_25_091708_create_livros_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('livros', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');
+            $table->string('autor');
+            $table->string('isbn')->unique();
+            $table->string('categoria')->nullable();
+            $table->year('ano_publicacao')->nullable();
+            $table->string('imagem')->nullable();
+            $table->enum('estado', ['disponivel', 'emprestado'])->default('disponivel');
+            $table->timestamps();
+});
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('livros');
+    }
+};
